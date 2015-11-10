07bdddb3a8cf1ba7a608d531ca20f5df1475f7a0
chinnadhurai <chinnadhurai@gmail.com>
2015-11-10 17:53:35 -0500

    parser to plot1

--- model_protoype/exp_chinna/parser_for_alex.py
+++ model_protoype/exp_chinna/parser_for_alex.py
@@ -20,51 +20,63 @@ def conv_number(s):
     except ValueError:
         return 0
 
+def get_config():
+    #data_root = '/u/lambalex/DeepLearning/ImportanceSampling/logs/'
+    data_root = '/Users/chinna/ImportanceSamplingSGD_alex/logging/'
+    config = {}
+    config['files'] = {'usgd': data_root + 'log_1447166866_.txt',
+                       'isgd': data_root + 'log_1447167499_.txt',
+                       'isgd_bug' : data_root + 'log_1447167499_.txt'}
+    config['plot'] = data_root + 'plot.jpeg'
+    return config
+
 class parser_class:
 
-    def __init__(self,file):
+    def __init__(self):
         print "creating parser object"
-        self.L_accuracy = []
-        self.L_mini_batch =[]
-        self.file = file
+        self.L_accuracy = {}
+        self.config = get_config()
 
-    def update_L_accuracy(self,line):
+    def update_L_accuracy(self,line,L_accuracy):
         match = re.search(r'\s*(accuracy)\s*:\s*(mean)\s*(?P<accuracy>\S*)\s*,',line)
         if match:
             acc = match.group('accuracy')
-            self.L_accuracy.append(conv_number(acc))
+            L_accuracy.append(conv_number(acc))
 
-    def update_L_mini_batch(self,line):
+    def update_L_mini_batch(self,line,L_mini_batch):
         match = re.search(r'Number minibatches processed by master\s*(?P<mini_batch_num>\S*)',line)
         if match:
             acc = match.group('mini_batch_num')
-            self.L_mini_batch.append(conv_number(acc))
+            L_mini_batch.append(conv_number(acc))
+
+    def parse_file(self,file):
+        fd = open(file,'r')
+        L_accuracy = []
+        for line in fd:
+            self.update_L_accuracy(line,L_accuracy)
+            #self.update_L_mini_batch(line)
+        print "finished parsing",file
+        return L_accuracy
 
     def parse(self):
-        fd = open(self.file,'r')
-        for line in fd:
-            self.update_L_accuracy(line)
-            self.update_L_mini_batch(line)
-        print "finished parsing"
+        for f_type,file in self.config['files'].items():
+            self.L_accuracy[f_type] = self.parse_file(file)
 
     def plot(self):
         #match = re.match(r'(log_)(\S).txt',self.file)
         #plt_file = match.group(2)
-        plt_file = '1'
-        plt_file = 'plot'+ plt_file + '.jpeg'
-        plt.plot(self.L_accuracy[0::3])
-        plt.plot(self.L_accuracy[1::3])
-        plt.plot(self.L_accuracy[2::3])
-        plt.savefig(plt_file)
-        print "plot saved to",plt_file
+        for k,v in self.L_accuracy.items():
+            plt.plot(v, label=k)
+        plt.legend()
+        plt.savefig(self.config['plot'])
+        print "plot saved to",self.config['plot']
 
     def run(self):
         self.parse()
         self.plot()
 
 if __name__ == "__main__":
-    file = "/Users/chinna/ImportanceSamplingSGD_alex/logging/log_1447166866_.txt"
-    parser = parser_class(file)
+    parser = parser_class()
     parser.run()
 
 
