02cab8e4f1a876ea7a34e07fa0fae4626f66d155
Alex Lamb <lambalex@leto50.iro.umontreal.ca>
2015-11-23 23:08:13 -0500

    Added scripts for running code, for convenience

---
+++ integration_distributed_training/scripts/run_scripts.sh
@@ -0,0 +1,5 @@
+python run_database.py --config_file="../config_files/config_lamb_01.py" --bootstrap_file="bootstrap_09232"
+
+python run_master.py --config_file="../config_files/config_lamb_01.py" --bootstrap_file="bootstrap_09232"
+
+python run_worker.py --config_file="../config_files/config_lamb_01.py" --bootstrap_file="bootstrap_09232"
