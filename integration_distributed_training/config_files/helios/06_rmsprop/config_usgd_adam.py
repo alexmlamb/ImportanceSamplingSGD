fefc99825568282f02095604ccefed1d250f7a26
chinnadhurai <chinnadhurai@gmail.com>
2015-11-30 01:13:43 -0500

    helios config files

--- integration_distributed_training/config_files/helios/06_rmsprop/config_usgd_adam.py
+++ integration_distributed_training/config_files/helios/06_rmsprop/config_usgd_adam.py
@@ -83,7 +83,7 @@ def get_database_config():
     # be willing to wait for the parameters to be present on the server.
     connection_setup_timeout = 10*60
 
-    experiment_root_dir = "/rap/jvb-000-aa/data/chinna/iclr2015/logging/rms_prop_exp/usgd_r"
+    experiment_root_dir = "/rap/jvb-000-aa/data/chinna/iclr2015/logging/adam_exp/usgd"
     redis_rdb_path_plus_filename = os.path.join(experiment_root_dir, "uniform.rdb")
     logging_folder = experiment_root_dir
 
